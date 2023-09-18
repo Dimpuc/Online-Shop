afd73090564f359c3d10f2f10f5b3bdee068e7aa
Dima Osadchy <osadchyd05@gmail.com>
2023-09-18 20:43:26 +0300

    HOT FIX

--- src/pages/catalog-page/index.tsx
+++ src/pages/catalog-page/index.tsx
@@ -1,37 +1,40 @@
+import { lazy } from "react";
 import { Box, Grid } from "@mui/material";
-import { ProductsList } from "../../components/products-list";
+
 import { FilterSidebar } from "../../components/filter-sidebar";
 import { CastomLoader } from "../../components/loader";
 import { useLoader } from "../../providers/LoaderProvider";
+import ProductsList from "../../components/products-list";
 
 const CatalogPage = () => {
   const { openLoader } = useLoader();
 
   return (
     <>
-      {openLoader ? (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "60vh",
-          }}
-        >
-          <CastomLoader />
-        </Box>
-      ) : (
-        <Grid
-          sx={{
-            display: "flex",
-            gap: "10px",
-            padding: "20px",
-          }}
-        >
-          <FilterSidebar />
+      <Grid
+        sx={{
+          display: "flex",
+          gap: "10px",
+          padding: "20px",
+        }}
+      >
+        <FilterSidebar />
+        {openLoader ? (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "60vh",
+              width: "100%",
+            }}
+          >
+            <CastomLoader />
+          </Box>
+        ) : (
           <ProductsList />
-        </Grid>
-      )}
+        )}
+      </Grid>
     </>
   );
 };
